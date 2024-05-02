f86fa8dcf6d26aca3b5b085ad0089200d7d22886
Aron Tiselius <aron.tiselius@gmail.com>
2024-05-02 21:36:20 +0200

    fixed more comments

--- pointOfSale/src/integration/InventoryHandler.java
+++ pointOfSale/src/integration/InventoryHandler.java
@@ -11,12 +11,19 @@ import model.Receipt;
 public class InventoryHandler {
 	ArrayList<ProductDTO> products = new ArrayList<ProductDTO>();
 
+	/**
+	 * Creates an instance of InventoryHandler. Currently only used for testing.
+	 */
 	public InventoryHandler() {
 		products.add(new ProductDTO(123, 10, 12, "apple"));
 		products.add(new ProductDTO(321, 5, 12, "banana"));
 	}
 
-	// search database, return product
+	/**
+	 * Searches the database and returns the product if it exists.
+	 * @param id of the product to fetch.
+	 * @return ProductDTO, the ProductDTO to return. Is null if it does not exist.
+	 */
 	public ProductDTO getProduct(int id) {
 		for (ProductDTO product : this.products) {
 			if (product.getId() == id)
@@ -28,7 +35,7 @@ public class InventoryHandler {
 	/**
 	 * Updates the inventory system. TODO implement
 	 * 
-	 * @param receipt
+	 * @param receipt that is sent to the inventory system.
 	 */
 	public void postReceipt(Receipt receipt) {
 		// TODO Auto-generated method stub
