08e1567b5a90e75abe56405481326c0846d8ffd9
William Redgård <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>
2024-05-02 17:40:51 +0200

    added test for getItem

--- pointOfSale/src/model/SaleTest.java
+++ pointOfSale/src/model/SaleTest.java
@@ -14,7 +14,8 @@ import org.junit.jupiter.api.Test;
 import dto.ProductDTO;
 
 class SaleTest {
-	 Sale sale;
+	Sale sale;
+
 	@BeforeEach
 	void setUp() throws Exception {
 		sale = new Sale();
@@ -30,72 +31,80 @@ class SaleTest {
 	@Test
 	void testItemNotNull() {
 		assertNotNull(sale, "Expected that item was not null, but it was.");
-		
-			
-		
 	}
-	
+
 	@Test
 	void canAddProduct() {
 		ProductDTO testProduct = new ProductDTO(1, 99.99f, 12, "Green apple");
 		sale.addProduct(testProduct);
 		Item item = sale.getItem(testProduct);
 
-		if(item == null) fail("is null");
-		
+		if (item == null)
+			fail("is null");
+
 		boolean expectedResult = true;
 		boolean result = item.getProduct().equals(testProduct) && item.getQuantity() == 1;
-		
+
 		assertEquals(expectedResult, result, "The products are not equal or the quantity is not 1");
 	}
-	
+
 	@Test
 	void canIncrementItem() {
-		int timesToIncrement = (int)( Math.random() * 1000);
+		int timesToIncrement = (int) (Math.random() * 1000);
 		ProductDTO testProduct = new ProductDTO(1, 99.99, 12, "Green apple");
 
-		
-		for(int i = 0; i < timesToIncrement; i++) {
+		for (int i = 0; i < timesToIncrement; i++) {
 			sale.addProduct(testProduct);
 		}
 		Item item = sale.getItem(testProduct);
-		if(item == null) fail("is null");
+		if (item == null)
+			fail("is null");
 		boolean expectedResult = true;
 		boolean result = item.getProduct().equals(testProduct) && item.getQuantity() == timesToIncrement;
-		
-		assertEquals(expectedResult, result, "The product is not equal or the quantity did not increment properly");
-	}	
 
-	// Extensive test that generates a long array of random products, then increments that product a random amount of times, meanwhile
-	// the total cost is kept track of. The test finally sees if the calculateRunning total matches with the expected total.
+		assertEquals(expectedResult, result, "The product is not equal or the quantity did not increment properly");
+	}
+
+	// Extensive test that generates a long array of random products, then
+	// increments that product a random amount of times, meanwhile
+	// the total cost is kept track of. The test finally sees if the
+	// calculateRunning total matches with the expected total.
 	@Test
 	void canCalculateCorrectTotalAmount() {
 		int maxItemsPossibleToFitInsideANormalStore = 10000;
-		int amountOfProducts =(int) (Math.random() * maxItemsPossibleToFitInsideANormalStore);
+		int amountOfProducts = (int) (Math.random() * maxItemsPossibleToFitInsideANormalStore);
 		ProductDTO[] products = new ProductDTO[amountOfProducts];
-		int reasonableMaxPriceForAProduct =  2000;
-		for(int i = 0; i < amountOfProducts; i++){
+		int reasonableMaxPriceForAProduct = 2000;
+		for (int i = 0; i < amountOfProducts; i++) {
 
-			products[i] = new ProductDTO(i, Math.random() * reasonableMaxPriceForAProduct, 12, "green " + String.valueOf(Math.random()));
+			products[i] = new ProductDTO(i, Math.random() * reasonableMaxPriceForAProduct, 12,
+					"green " + String.valueOf(Math.random()));
 		}
 
 		Double totalAmount = 0.0;
 		int reasonableMaximumAmountOfStockInStore = 100;
-		for(ProductDTO product : products) {
-			int timesToIncrement = (int) (( Math.random() * reasonableMaximumAmountOfStockInStore));
-			for(int i = 0; i < timesToIncrement; i++) {
+		for (ProductDTO product : products) {
+			int timesToIncrement = (int) ((Math.random() * reasonableMaximumAmountOfStockInStore));
+			for (int i = 0; i < timesToIncrement; i++) {
 				totalAmount += product.getPrice();
 				sale.addProduct(product);
 			}
-			
-			
-		};
-		//Margin of failure
+
+		}
+		;
+		// Margin of failure
 		double epsilon = 0.001d;
-		assertEquals(totalAmount, sale.getRunningTotal(), epsilon,"Could not count: Got: " + sale.getRunningTotal() + ", but expected: " + totalAmount );
+		assertEquals(totalAmount, sale.getRunningTotal(), epsilon,
+				"Could not count: Got: " + sale.getRunningTotal() + ", but expected: " + totalAmount);
 
 	};
-	
 
+	@Test
+	void canGetItem() {
+		ProductDTO testProduct = new ProductDTO(1, 99.99f, 12, "Green apple");
+		sale.addProduct(testProduct);
+		Item item = sale.getItem(testProduct);
+		assertEquals(item.getProduct(), testProduct, "products are not same");
+	}
 
 }
